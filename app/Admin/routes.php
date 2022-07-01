e4d4b1c557c07ee63dab4eab53436fcfa3f0864d
csaralbrto <csaralbrto@gmail.com>
2022-07-01 15:51:35 -0500

    second commit

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -16,6 +16,4 @@ Route::group([
     $router->resource('cuentas', AccountsController::class);
     $router->resource('dayli_rate', PricingDayController::class);
     $router->resource('casa_cambio', AdministratorController::class);
-
-
 });
